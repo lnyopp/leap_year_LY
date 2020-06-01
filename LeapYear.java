b69388d0c4ac441754233425abddb8d47f1db94b
Rumble Roar <lnyopp15@gmail.com>
2020-06-01 17:36:38 -0400

    JUNIT testing of leap year

---
+++ LeapYear.java
@@ -0,0 +1,30 @@
+public class LeapYear {
+	
+	int year;
+	
+	// divide by 4
+	public static boolean divideBy4(int year){
+		if( (year%4) == 0) {
+			return true;
+		} else 
+			
+		{ return false;
+		
+		}
+		
+	}
+	
+	
+	public static boolean hundredandfourhundred(int year){
+		if( (year%100) == 0 && (year%400) == 0) {
+			return true;
+		} else 
+			
+		{ return false;
+		
+		}
+		
+	}
+	
+		
+	}
---
+++ LeapYearTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+
+public class LeapYearTest {
+	
+	
+	  @Test
+	    public final void isYearDivisibleByFour() {
+	       
+		  LeapYear.divideBy4(2020);
+		  Assert.assertTrue(true);
+	    }
+	  
+	  @Test
+	    public final void isYearDivisibleBy100and400() {
+	       
+		  LeapYear.hundredandfourhundred(1999);
+		  Assert.assertTrue(true);
+	    }
+	  
+}
